9f34aa01538cdfcc470830bed8845e565c0c9381
I <i>
2024-06-08 09:01:20 +0000

    clean repository

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    JWT_EXPIRATION_DELTA = 3600  # Token expiration time in seconds (e.g., 1 hour)
--- models.py
+++
@@ -1,4 +0,0 @@
-employees = {
-    "john_doe": {"password": "password123", "salary": 50000, "next_raise_date": "2024-12-01"},
-    "jane_doe": {"password": "password456", "salary": 55000, "next_raise_date": "2024-11-15"}
-}
